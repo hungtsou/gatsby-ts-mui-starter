dad6b4536b01624f9c408518c0bd78d91d6ba274
hungtsou <hungtsougg@gmail.com>
2019-08-06 21:25:27 -0600

    Added mui addon to storybook

--- stories/button.stories.tsx
+++ stories/button.stories.tsx
@@ -2,12 +2,24 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-
-import { Button } from '@storybook/react/demo'
+// @ts-ignore
+import { muiTheme } from 'storybook-addon-material-ui'
+import Button from '@material-ui/core/Button'
+import theme from '../src/lib/theme'
 
 export default () =>
   storiesOf('Button', module)
-    .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+    .addDecorator(muiTheme(theme))
+    .add('primary', () => (
+      <Button variant="contained" color="primary" onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    ))
+    .add('secondary', () => (
+      <Button variant="contained" color="secondary" onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    ))
     .add('with some emoji', () => (
       <Button onClick={action('clicked')}>
         <span role="img" aria-label="so cool">
