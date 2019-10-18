6586d04e2460cb7d88b56c5d1f576a4f3fcd63cb
hungtsou <hungtsougg@gmail.com>
2019-10-18 12:14:28 -0600

    fix: add padding left to main content

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -12,7 +12,13 @@ const Layout: React.FC = ({ children }) => (
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
     </Helmet>
-    {children}
+    <div
+      style={{
+        paddingLeft: '100px'
+      }}
+    >
+      {children}
+    </div>
   </ThemeProvider>
 )
 
